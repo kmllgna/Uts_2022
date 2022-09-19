bf917f55b58a3c2753c3036d9b55c97e8bdf5410
kmllgna <113911813+kmllgna@users.noreply.github.com>
2022-09-19 20:38:37 +0700

    Add files via upload

---
+++ Kamal legiana_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama  :Guntur"<<endl;
+  
+    cout << "Kelas :X PPLG 2"<<endl;
+    cout << "Nilai :85"<<endl;
+    return 0;
+    
+}
